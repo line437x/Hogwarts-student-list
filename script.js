bf0250fecdcfa80a7e4e46946966e27fe88b731d
lineberner <line437x@stud.kea.dk>
2022-02-25 14:07:28 +0100

    filtering done

--- script.js
+++ script.js
@@ -2,7 +2,7 @@
 
 window.addEventListener("DOMContentLoaded", setup);
 //------------------- Variables -------------------
-let students = [];
+let allStudents = [];
 
 const Student = {
   prefect: false,
@@ -19,6 +19,7 @@ const settings = {
 //------------------- Setup -------------------
 function setup() {
   console.log("script er loaded");
+  registerButtons();
   loadJSON();
 }
 
@@ -26,70 +27,85 @@ function setup() {
 async function loadJSON() {
   const url = "https://petlatkea.dk/2021/hogwarts/students.json";
   let data = await fetch(url);
-  students = await data.json();
+  allStudents = await data.json();
 
-  //   console.table(students);
-
-  createStudents(students);
+  //   console.table(allStudents);
+  createStudents(allStudents);
+}
+function createStudents(data) {
+  allStudents = data.map(prepareObject);
+  //console.log("allStudents", allStudents);
+  displayList(allStudents);
+  //displayStudent(allStudents);
+}
+//------------------- Create buttons -------------------
+function registerButtons() {
+  document.querySelectorAll("[data-action='filter']").forEach((button) => button.addEventListener("click", selectFilter));
+  document.querySelectorAll("[data-action='sort']").forEach((button) => button.addEventListener("click", selectSort));
 }
 //------------------- create students -------------------
-function createStudents() {
-  students.forEach((object) => {
-    // Define a template for the data objects
-    const Student = {
-      firstName: "",
-      lastName: "",
-      middleName: "",
-      nickName: "",
-      image: "",
-      house: "",
-    };
-    // create a objects from a prototype
-    const student = Object.create(Student);
 
-    //Trim objects
-    let originalName = object.fullname.trim();
+function prepareObject(object) {
+  //  allStudents.forEach((object) => {
+  // Define a template for the data objects
+  const Student = {
+    firstName: "",
+    lastName: "",
+    middleName: "",
+    nickName: "",
+    image: "",
+    house: "",
+  };
+  // create a objects from a prototype
+  const student = Object.create(Student);
 
-    // ----- First name -----
-    if (originalName.includes(" ")) {
-      student.firstName = originalName.substring(0, 1).toUpperCase() + originalName.substring(1, originalName.indexOf(" "));
-    } else {
-      student.firstName = originalName.substring(0, 1).toUpperCase() + originalName.substring(1);
-    }
-    student.firstName = student.firstName.substring(0, 1).toUpperCase() + student.firstName.substring(1).toLowerCase();
+  //Trim objects
+  let originalName = object.fullname.trim();
 
-    // ----- Last name -----
-    if (originalName.includes(" ")) {
-      student.lastName = originalName.substring(originalName.lastIndexOf(" ") + 1);
-      student.lastName = student.lastName.substring(0, 1).toUpperCase() + student.lastName.substring(1).toLowerCase();
-    }
+  // ----- First name -----
+  if (originalName.includes(" ")) {
+    student.firstName = originalName.substring(0, 1).toUpperCase() + originalName.substring(1, originalName.indexOf(" "));
+  } else {
+    student.firstName = originalName.substring(0, 1).toUpperCase() + originalName.substring(1);
+  }
+  student.firstName = student.firstName.substring(0, 1).toUpperCase() + student.firstName.substring(1).toLowerCase();
 
-    //----- Middle name (if any) -----
-    student.middleName = originalName.substring(originalName.indexOf(" ") + 1, originalName.lastIndexOf(" "));
-    student.middleName = student.middleName.substring(0, 1).toUpperCase() + student.middleName.substring(1).toLowerCase();
+  // ----- Last name -----
+  if (originalName.includes(" ")) {
+    student.lastName = originalName.substring(originalName.lastIndexOf(" ") + 1);
+    student.lastName = student.lastName.substring(0, 1).toUpperCase() + student.lastName.substring(1).toLowerCase();
+  }
 
-    //----- Nick name (if any) -----
-    if (originalName.includes('"')) {
-      student.middleName = "";
-      student.nickName = originalName.substring(originalName.indexOf('"'), originalName.lastIndexOf('"') + 1);
-    }
-    // // ----- Image -----
-    student.image = `./images/${originalName.substring(0, originalName.indexOf(" ")).toLowerCase()}_.png`;
-    student.image = `./images/${originalName.substring(originalName.lastIndexOf(" ") + 1, originalName.lastIndexOf(" ") + 2).toLowerCase() + originalName.substring(originalName.lastIndexOf(" ") + 2).toLowerCase()}_${originalName
-      .substring(0, 1)
-      .toUpperCase()
-      .toLowerCase()}.png`;
+  //----- Middle name (if any) -----
+  student.middleName = originalName.substring(originalName.indexOf(" ") + 1, originalName.lastIndexOf(" "));
+  student.middleName = student.middleName.substring(0, 1).toUpperCase() + student.middleName.substring(1).toLowerCase();
 
-    // ----- House -----
-    let originalHouse = object.house.trim();
-    student.house = originalHouse;
-    student.house = student.house.substring(0, 1).toUpperCase() + student.house.substring(1).toLowerCase();
-    // console.table(student);
+  //----- Nick name (if any) -----
+  if (originalName.includes('"')) {
+    student.middleName = "";
+    student.nickName = originalName.substring(originalName.indexOf('"'), originalName.lastIndexOf('"') + 1);
+  }
+  // // ----- Image -----
+  student.image = `./images/${originalName.substring(0, originalName.indexOf(" ")).toLowerCase()}_.png`;
+  student.image = `./images/${originalName.substring(originalName.lastIndexOf(" ") + 1, originalName.lastIndexOf(" ") + 2).toLowerCase() + originalName.substring(originalName.lastIndexOf(" ") + 2).toLowerCase()}_${originalName
+    .substring(0, 1)
+    .toUpperCase()
+    .toLowerCase()}.png`;
 
-    displayStudent(student);
-  });
+  // ----- House -----
+  let originalHouse = object.house.trim();
+  student.house = originalHouse;
+  student.house = student.house.substring(0, 1).toUpperCase() + student.house.substring(1).toLowerCase();
+  // console.table(student);
+  return student;
+  //displayStudent(student);
+  //});
+  // displayList(allStudents);
 }
-
+function displayList(list) {
+  document.querySelector("tbody").innerHTML = "";
+  list.forEach((student) => displayStudent(student));
+}
 //------------------- Display students -------------------
 function displayStudent(student) {
   const clone = document.querySelector("template#student").content.cloneNode(true);
@@ -102,38 +118,77 @@ function displayStudent(student) {
 }
 
 //------------------- All filter functions -------------------
-function filterGryffindor() {}
-function filterSlytherin() {}
-function filterHufflepuff() {}
-function filterRavenclaw() {}
-function filterPrefects() {}
-function filterExpelled() {}
-function filterNonExpelled() {}
-function filterSquad() {}
-function filterBoys() {}
-function filterGirls() {}
-function filterPureBlood() {}
-function filterHalfBlood() {}
-function filterMuggle() {}
-function removeFilter() {}
+function filterList(filterBy) {
+  let filteredList = allStudents;
+  // console.log(filteredList);
 
-//------------------- All sort functions -------------------
-function sortFirstName() {}
-function sortLastName() {}
-function sortHouse() {}
-function sortPrefects() {}
+  if (filterBy === "gryffindor") {
+    filteredList = allStudents.filter(filterGryffindor);
+    console.log(filteredList);
+  } else if (filterBy === "slytherin") {
+    filteredList = allStudents.filter(filterSlytherin);
+    console.log(filteredList);
+  } else if (filterBy === "hufflepuff") {
+    filteredList = allStudents.filter(filterHufflepuff);
+    console.log(filteredList);
+  } else if (filterBy === "ravenclaw") {
+    filteredList = allStudents.filter(filterRavenclaw);
+    console.log(filteredList);
+  }
+  displayList(filteredList);
+}
 
-//------------------- Search function -------------------
-function search() {}
+function selectFilter(event) {
+  const filter = event.target.dataset.filter;
+  console.log(filter);
+  filterList(filter);
+}
 
-//------------------- All toogle functions -------------------
-function makePrefect() {}
-function undoPrefect() {}
-function makeSquadMember() {}
+function filterGryffindor(student) {
+  return student.house === "Gryffindor";
+}
+function filterSlytherin(student) {
+  return student.house === "Slytherin";
+}
+function filterHufflepuff(student) {
+  return student.house === "Hufflepuff";
+}
+function filterRavenclaw(student) {
+  return student.house === "Ravenclaw";
+}
 
-//------------------- All non reversible functions -------------------
-function expelStudent() {}
-function hackSystem() {}
+function selectSort() {
+  console.log("sorterknap");
+}
 
-//------------------- Pop-up -------------------
-function showStudentDetails() {}
+// function filterPrefects() {}
+// function filterExpelled() {}
+// function filterNonExpelled() {}
+// function filterSquad() {}
+// function filterBoys() {}
+// function filterGirls() {}
+// function filterPureBlood() {}
+// function filterHalfBlood() {}
+// function filterMuggle() {}
+// function removeFilter() {}
+
+// //------------------- All sort functions -------------------
+// function sortFirstName() {}
+// function sortLastName() {}
+// function sortHouse() {}
+// function sortPrefects() {}
+
+// //------------------- Search function -------------------
+// function search() {}
+
+// //------------------- All toogle functions -------------------
+// function makePrefect() {}
+// function undoPrefect() {}
+// function makeSquadMember() {}
+
+// //------------------- All non reversible functions -------------------
+// function expelStudent() {}
+// function hackSystem() {}
+
+// //------------------- Pop-up -------------------
+// function showStudentDetails() {}
